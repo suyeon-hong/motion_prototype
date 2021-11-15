e312ad0153488b7b0519f69f8388e92bbb68f68c
bellahong95 <bellahong95@gmail.com>
2021-11-15 20:14:39 +0900

    img 클릭 모션 추가

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
 const $sliderWrap = $(".slider-wrap");
 const $slider = $(".slider");
 const $pagebar = $(".bar");
-const $prev = $(".prev");
-const $next = $(".next");
+const $prev = $(".wrap .prev");
+const $next = $(".wrap .next");
+const $img = $slider.find(".bg");
+const $detail = $(".detail");
+const $pic = $detail.find(".pic");
+const $close = $detail.find(".close");
 const len = $slider.length - 3;
 let num = 1;
 let enableClick = true;
+let index;
+let picPos = [
+    {
+        top: "300px",
+        left: "290px",
+        width: "25vw",
+        height: "20vw"
+    },
+    {
+        top: "250px",
+        left: "720px",
+        width: "25vw",
+        height: "20vw"
+    },
+    {
+        top: "290px",
+        left: "1150px",
+        width: "25vw",
+        height: "20vw"
+    },
+]
 
-console.log(len);
 $prev.on("click", function(e){
     e.preventDefault();
 
@@ -24,6 +48,36 @@ $next.on("click", function(e){
         enableClick = false;
         next();
     }
+});
+
+$img.on("click", function(){
+    index = $(this).parent(".slider").index();
+
+    if(index > 2) index = index - num + 1;
+    
+    $img.eq(index).css({opacity: 0});
+    $pic.css(picPos[index]);
+    setTimeout(function(){
+        $pic.css({
+            top: 0,
+            left: "10vw",
+            width: "40vw",
+            height: "100%"
+        });
+    }, 100);
+    setTimeout(function(){
+        $(".wrap").addClass("on");
+    }, 500);
+    $detail.fadeIn(1000);
+});
+
+$close.on("click", function(e){
+    e.preventDefault();
+
+    $img.eq(index).css({opacity: 1});
+    $pic.css(picPos[index]);
+    $(".wrap").removeClass("on");
+    $detail.fadeOut(1000);
 });
 
 function prev(){
